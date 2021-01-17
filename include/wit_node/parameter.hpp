a4d8fa70af88ae30f18acd387888cdc5834cec4e
Ibrahim Salman <38477627+18r441m@users.noreply.github.com>
2021-01-17 17:24:34 -0500

    Update to default BaudRate

--- include/wit_node/parameter.hpp
+++ include/wit_node/parameter.hpp
@@ -5,7 +5,7 @@ using namespace std;
 namespace wit {
 class Parameter {
  public:
-  Parameter() : port_("/dev/ttyUSB0"), baut_rate_(115200), ns("wit") {}
+  Parameter() : port_("/dev/ttyUSB0"), baut_rate_(9600), ns("wit") {}
 
   string port_;
   int baut_rate_;
